c83283577d068c59cd55906e358a65cdb69afdf0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-30 17:46:11 +0900

    added ua for avoid warnings

--- script/bench.pl
+++ script/bench.pl
@@ -4,7 +4,7 @@ use HTTP::Message::PSGI;
 use HTTP::Request;
 use Benchmark ':all';
 
-my $req = HTTP::Request->new(GET => 'http://127.0.0.1/');
+my $req = HTTP::Request->new(GET => 'http://127.0.0.1/', ['User-Agent' => 'hogehoge']);
 
 my $config = do 'config.pl';
 my $app = Inamode13::Web->to_app(config => $config);
