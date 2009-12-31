a9ae24ac01e985169a53cb4887c595d2977f1fcb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-01-01 00:13:32 +0900

    support -c for regen-cache.pl

--- script/regen-cache.pl
+++ script/regen-cache.pl
@@ -1,10 +1,19 @@
 use strict;
 use warnings;
+use File::Spec;
+use FindBin;
+use lib File::Spec->catfile($FindBin::Bin, '..', 'lib');
 use Inamode13::Formatter;
 use Inamode13;
 use Amon::Declare;
+use Getopt::Long;
 
-my $config = do 'config.pl' or die $@;
+my $conffile = 'config.pl';
+GetOptions(
+    'c|config=s' => \$conffile,
+);
+
+my $config = do $conffile or die $@;
 my $c = Inamode13->bootstrap(config => $config);
 my $entry_iter = $c->model('DB')->search('entry');
 while (my $entry = $entry_iter->next) {
