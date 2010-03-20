75121c78480d09c3fc49c6f4d651b954ca596934
tokuhirom <tokuhirom@gmail.com>
2010-03-21 03:56:45 +0900

    use latest Router::Simple

--- lib/Inamode13/Web/Dispatcher.pm
+++ lib/Inamode13/Web/Dispatcher.pm
@@ -3,7 +3,7 @@ use Amon::Web::Dispatcher::RouterSimple -base;
 
 connect '/',          {controller => 'Root',  action => 'index'};
 connect '/entry',     {controller => 'Entry', action => 'add'};
-submapper('/entry/{entry_id:[0-9]+}', controller => 'Entry')
+submapper('/entry/{entry_id:[0-9]+}', {controller => 'Entry'})
     ->connect('',         {action => 'show'})
     ->connect('/edit',    {action => 'edit'})
     ->connect('/history', {action => 'history'})
